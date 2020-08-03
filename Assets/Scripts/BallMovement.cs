09f543e70d5ba937afd9d7b9bf1527b1cb20f82d
Matey-Git <mat.michalak1999@gmail.com>
2020-08-03 16:11:56 +0200

    fan and ball movements

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] private Transform fanTransform = default;
+
+    private Rigidbody rb = default;
+    private Vector3 destination = default;
+    private bool powerOn = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            powerOn = !powerOn;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (powerOn)
+        {
+            destination = -(fanTransform.position - transform.position);
+
+            rb.AddForce(destination * 6, ForceMode.Acceleration);
+        }
+    }
+}
---
+++ Assets/Scripts/FanMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class FanMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private Transform target = default;
+
+    private float horizontal = default;
+    private float fanAngle = default;
+
+    private void Update()
+    {
+        transform.position = target.position;
+
+        horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        fanAngle -= horizontal;
+        transform.localRotation = Quaternion.Euler(0, fanAngle, 0);
+
+    }
+}
